c1e673584f7e98e49f312496bb7f8baa9bc6db98
theryanclausen <theryanclausen@gmail.com>
2018-12-20 08:26:03 -0700

    webpack boilerplate

---
+++ src/scenes/simple-scene.js
@@ -0,0 +1,9 @@
+export class SimpleScene extends Phaser.Scene{
+    preload(){
+        this.load.image('cokecan', 'assets/cokecan.png')
+    }
+    create(){
+        this.add.text(100,100, 'Sup doo', {fill: '#0f0'})
+        this.add.image(100,200, 'cokecan')
+    }
+}
